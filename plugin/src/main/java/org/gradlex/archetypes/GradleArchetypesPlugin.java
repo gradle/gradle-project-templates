000092ef4656983934fed20c44bd381d954329b1
Donat Csikos <donat@gradle.com>
2022-07-26 15:07:06 +0200

    Cleanup and todo

--- plugin/src/main/java/org/gradlex/archetypes/GradleArchetypesPlugin.java
+++ plugin/src/main/java/org/gradlex/archetypes/GradleArchetypesPlugin.java
@@ -8,11 +8,6 @@ import org.gradle.api.Project;
 
 public class GradleArchetypesPlugin implements Plugin<Project> {
     public void apply(Project project) {
-        // Register a task
-        project.getTasks().register("greeting", task -> {
-            task.doLast(s -> System.out.println("Hello from plugin 'org.gradlex.archetypes.greeting'"));
-        });
-
         if (project.getParent() == null) {
             project.afterEvaluate(project1 -> project1.getTasks().replace("init", GenerateProjectTask.class));
         }
---
+++ plugin/src/main/java/org/gradlex/archetypes/TemplateGeneration.java
@@ -0,0 +1,5 @@
+package org.gradlex.archetypes;
+
+public class TemplateGeneration {
+    // TODO this class should iterate files and invoke TemplateEngine callbacks
+}
